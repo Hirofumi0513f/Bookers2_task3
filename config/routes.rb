14e3c4606c6f69b60640545163728a847ec1c4fb
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-11-26 09:10:10 +0000

    [Update]bookers2_CK17,18,19_2022-1126-1810

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'books/edit'
   get 'books/update'
   get 'books/destroy'
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :update]
   get '/homes/about', to: "homes#about", as: "about"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
