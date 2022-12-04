6b7fcf3c1d1c3fb6747e24592d33848fb3beac49
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-04 00:56:18 +0000

    [Update]bookers2_2022-1204-0956

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :is_matching_login_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -7,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @book = Book.new
+    @users = User.all
   end
 
   def edit
