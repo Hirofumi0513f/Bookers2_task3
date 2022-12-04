ef68bf761548379afaa76c04b5616516204235b7
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-04 06:31:58 +0000

    [Update]bookers2_2022-1204-1531

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -37,7 +36,7 @@ class UsersController < ApplicationController
     user_id = params[:id].to_i
     login_user_id = current_user.id
     if(user_id != login_user_id)
-      redirect_to post_images_path
+      redirect_to user_path(current_user.id)
     end
   end
 end
