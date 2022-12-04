a71a21e48fd0f0d206a9e3a1944d4faf83f3d8c5
HirofumiFukuda <117177547+Hirofumi0513f@users.noreply.github.com>
2022-12-04 08:11:54 +0000

    [Update]bookers2_2022-1204-1711

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!,except: [:top, :about]
   before_action :configure_permitted_parameters,if: :devise_controller?
 
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Welcome! You have signed_up successfully."
+    books_path
+  end
+
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
-    user_path(current_user.id)
+    books_path
   end
 
   def after_sign_out_path_for(resource)
